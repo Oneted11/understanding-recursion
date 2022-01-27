225296714335c85416d5a04bf02095c3f14b5c7d
Oneted11 <muriithited@gmail.com>
2022-01-27 14:58:39 +0300

    tryed, need more reserch, not looping

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const myarr = [9, 8, 3, 6, 7, 4];
 const accumulator = (arr, acc) => {
   //do add to acc
   //   i = 1;
-  if (arr.length > 0) {
+  if (arr.length == 0) {
+    console.log(acc, arr.length);
+
+    return acc;
+  } else {
     acc = acc + Number(arr.shift());
 
     accumulator(arr, acc);
-  } else return acc;
+    console.log(acc, arr.length);
+  }
+  return acc;
 };
 const sum = 0;
 console.log("the ans=>", accumulator(myarr, sum));
