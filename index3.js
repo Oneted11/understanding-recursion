25f21eeee6ff477b284494d6d18ca34357b658b3
Oneted11 <muriithited@gmail.com>
2022-02-12 08:54:10 +0300

    cahd version working

--- index3.js
+++ index3.js
@@ -1,6 +1,10 @@
 const myarr = [9, 8, 3, 6, 7, 4];
 
 const adder = (arr) => {
-  return arr.pop() + adder(arr);
+  // return zero when array is finished
+  //doesnt return without it
+  if (arr.length === 0) return 0;
+  //call adder with the remaining array after adding the last item through popping
+  else return arr.pop() + adder(arr);
 };
 console.log(adder(myarr));
